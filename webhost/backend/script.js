e978770d6729614e68717761358ffd5edb7d8193
uthra44 <uthralakshmanan133@gmail.com>
2024-09-08 21:57:16 +0530

    Update script.js

--- webhost/backend/script.js
+++ webhost/backend/script.js
@@ -47,7 +47,7 @@ app.post('/submit', (req, res) => {
 
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
